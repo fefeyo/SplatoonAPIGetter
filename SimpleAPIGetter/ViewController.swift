c5de307ad50d5fafe56695b6c7f7545389253768
阪田祐宇 <skty.fefe@i.softbank.jp>
2016-03-05 17:23:31 +0900

    Initial Commit

---
+++ SimpleAPIGetter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAPIGetter
+//
+//  Created by 阪田祐宇 on 2016/03/05.
+//  Copyright © 2016年 fefe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
